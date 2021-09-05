e2cd85a941a653f9d4bd71b833aa947ecd536222
Waqas Anwar <waqasdotnet@hotmail.com>
2021-09-05 12:58:52 +0500

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFormsDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name must be less than 50 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+    } 
+}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorFormsDemo.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorFormsDemo.Pages
+{
+    public partial class Index
+    {
+        private Product ProductModel = new Product(); 
+        private EditContext EditContext;
+
+        protected override void OnInitialized()
+        {
+            EditContext = new EditContext(ProductModel);
+        }
+
+        private void HandleValidSubmit()
+        {
+            Console.WriteLine("HandleValidSubmit Called...");
+        }
+    } 
+}
